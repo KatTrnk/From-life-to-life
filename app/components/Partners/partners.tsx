361efda1b78176758f2d552824995b68ae495d2c
DarjaCodes <krjukova4darja@gmail.com>
2023-12-03 11:42:57 +0100

    Errors fixed <p>

--- app/components/Partners/partners.tsx
+++ app/components/Partners/partners.tsx
@@ -12,11 +12,11 @@ export const Partners: React.FC<PartnersProps> = ({
   title,
 }) => {
   return (
-    <>
+    <div>
       <img className="logo" src={logoUrl}></img>
       <p className="email">{email}</p>
       <p className="link">{url}</p>
       <p className="details">{title}</p>
-    </>
+    </div>
   );
 };
