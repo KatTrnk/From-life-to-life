a279a4fa3eb4baf5743efb8df6075809a4b7980e
Kateřina Trnková <kat.trnkova@gmail.com>
2023-12-03 10:45:02 +0100

    create component story

--- app/stories/[slug]/page.tsx
+++ app/stories/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Asset, Entry, EntryFieldTypes, createClient } from 'contentful';
 import { storyDataStructure } from '../page';
+import { StoryDetail } from '@/app/components/Story/story';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID || '',
@@ -19,6 +20,23 @@ const getStoryData = async (slug: string) => {
 export default async function Story({ params }: { params: { slug: string } }) {
   const storyData = await getStoryData(params.slug);
   console.log(storyData);
+  const mainImage = storyData.fields.mainImage as Asset<undefined, string>;
 
-  return <></>;
+  return (
+    <div>
+      <StoryDetail
+        title={storyData.fields.title}
+        name={storyData.fields.name}
+        cityFrom={storyData.fields.cityFrom}
+        cityTo={storyData.fields.cityTo}
+        mainImage={mainImage}
+        storyImages={storyData.fields.storyImages}
+        quote={storyData.fields.quote}
+        summary={storyData.fields.summary}
+        article={storyData.fields.article}
+        slug={storyData.fields.slug}
+        isHorizontal={false}
+      />
+    </div>
+  );
 }
